532fd7f7f6f320b3077da0d66552e3781db13043
CraterMore <ctm6704@yahoo.com>
2025-01-12 21:11:55 -0500

    Click event for pins on map

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import LocationEntry from './assets/components/LocationEntry';
-import { Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
+import { Map, AdvancedMarker, Pin, useMap } from '@vis.gl/react-google-maps';
 type Poi ={ key: string, location: google.maps.LatLngLiteral }
 
 const App = () => {
@@ -12,6 +12,8 @@ const App = () => {
   }
 
   const [locations, setLocations] = useState<any[]>([]);
+
+  const [selected, setSelected] = useState<string | null>(null);
 
   const [data, setData] = useState<NotionData | null>(null);
 
@@ -61,8 +63,9 @@ const App = () => {
           ))}
         </div>
       </div>
-      <div className="w-2/3 bg-blue-100">
-        <Map
+      <div className="w-2/3 bg-blue-100 relative flex flex-col justify-end px-4">
+        <div className="w-full h-full absolute inset-0">
+          <Map
           defaultZoom={14}
           defaultCenter={ { lat: 51.509865, lng: -0.118092 } }
           streetViewControl={false}
@@ -71,21 +74,43 @@ const App = () => {
           // onCameraChanged={ (ev: MapCameraChangedEvent) =>
           //   console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
           // }
-        >
-          <PoiMarkers pois={locations} />
-        </Map>
+          >
+            <PoiMarkers pois={locations} setSelected={setSelected} />
+            
+          </Map>
+          
+        </div>
+        <div className="bg-white drop-shadow-xl h-1/2 w-80 rounded-t-3xl relative z-10">
+          {selected}
+        </div>
       </div>
     </div>
   );
 };
 
-const PoiMarkers = (props: {pois: Poi[]}) => {
+
+
+const PoiMarkers = (props: {pois: Poi[], setSelected: (place: string | null) => void}) => {
+  const map = useMap();
+  
+  const handleClick = useCallback((ev: google.maps.MapMouseEvent) => {
+    if(!map) return;
+    if(!ev.latLng) return;
+    console.log('marker clicked:', ev.latLng.toString());
+    map.panTo(ev.latLng);
+  }, []);
+
   return (
     <>
       {props.pois.map( (poi: Poi) => (
         <AdvancedMarker
           key={poi.key}
-          position={poi.location}>
+          position={poi.location}
+          clickable={true}
+          onClick={(ev: google.maps.MapMouseEvent) => {
+            props.setSelected(poi.key);
+            handleClick(ev);
+          }}>
         <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
         </AdvancedMarker>
       ))}
